8a1afbc6cd6b0faacb283a02fa4654c0f0a79d13
Logan Morazain <lxmorazain@gmail.com>
2025-03-13 01:03:18 +0000

    Databases and Generic Systems Added

--- main.py
+++ main.py
@@ -3,21 +3,45 @@ import sqlite3
 class LibraryManagementSystem:
     def __init__(self):
         self.db = sqlite3.connect("lms.db")
+        self.cur = self.db.cursor()
+        
+        try:
+            self.cur.execute("CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, name, hash);")
+        except sqlite3.OperationalError as e:
+            print(e)
+        try:
+            self.cur.execute("CREATE TABLE items(id INTEGER PRIMARY KEY AUTOINCREMENT, ris);")
+        except sqlite3.OperationalError as e:
+            print(e)    
+        try:
+            self.cur.execute("CREATE TABLE loans(id INTEGER PRIMARY KEY AUTOINCREMENT, user, item, type, expires, FOREIGN KEY(item) REFERENCES items(id), FOREIGN KEY(user) REFERENCES users(id));")
+        except sqlite3.OperationalError as e:
+            print(e)
+
         self.ls = LoanSystem(self.db)
         self.ums = UserManagementSystem(self.db)
         self.ss = StockSystem(self.db)
 
-class LoanSystem:
+class GenericSystem:
     def __init__(self, db):
         self.db = db
         self.cur = self.db.cursor()
+    
+    def commit(self):
+        self.db.commit()
 
-class UserManagementSystem:
+class LoanSystem(GenericSystem):
     def __init__(self, db):
-        self.db = db
-        self.cur = self.db.cursor()
+        super().__init__(db)
 
-class StockSystem:
+       
+class UserManagementSystem(GenericSystem):
     def __init__(self, db):
-        self.db = db
-        self.cur = self.db.cursor()
+        super().__init__(db)
+
+class StockSystem(GenericSystem):
+    def __init__(self, db):
+        super().__init__(db)
+
+
+lms = LibraryManagementSystem()
